0cd4048e395563068bdaed9810fb2bf1ed895ae1
Kazi Anowar <arrowanwar@gmail.com>
2024-10-19 15:44:09 +0600

    up

---
+++ database/migrations/2024_10_19_092950_create_accuses.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accuses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('case_id')->constrained();
+            $table->foreignId('enquries_detail_id')->constrained();
+            $table->timestamp('create_at')->useCurrent();
+            $table->timestamp('update_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accuses');
+    }
+};
